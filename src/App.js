9bbe375ba8668d4bbdf31d23f61506d763bae05c
Lishan997 <mithilabcc07@gmail.com>
2022-10-31 02:17:48 +0530

    Learn-Dynamic Values in Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = 'welcom mithila';
+  const number = 50;
+  const person = {name:'lishan', age:25};
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='content'>
+        <h1>{title}</h1>
+
+        <h2>{number}</h2>
+        <h3>My age is {person.age}</h3>
+
+        <h4>{10+2}</h4>
+        <h4>{'hello ninja'}</h4>
+        <h4>{[1,2,3,4,5]}</h4>
+        <h4>{Math.random()*10}</h4>
+
+        <a href={link}>Google link</a>
+      </div> 
     </div>
   );
 }
