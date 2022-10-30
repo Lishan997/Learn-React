c686f44a5f63c7f4cffa8d7487f9b3c39ec32227
Lishan997 <mithilabcc07@gmail.com>
2022-10-31 02:33:04 +0530

    Create multiple components and import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from "./Home";
 
 function App() {
 
-  const title = 'welcom mithila';
-  const number = 50;
-  const person = {name:'lishan', age:25};
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
      <div className='content'>
-        <h1>{title}</h1>
-
-        <h2>{number}</h2>
-        <h3>My age is {person.age}</h3>
-
-        <h4>{10+2}</h4>
-        <h4>{'hello ninja'}</h4>
-        <h4>{[1,2,3,4,5]}</h4>
-        <h4>{Math.random()*10}</h4>
-
-        <a href={link}>Google link</a>
+        <Home />
       </div> 
     </div>
   );
