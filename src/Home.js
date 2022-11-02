5304d443a03e8b42f8eb7022af9dad3463704f23
Lishan997 <mithilabcc07@gmail.com>
2022-11-02 12:36:56 +0530

    Learn How to add css and how click functions works

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+
+    const handleclick = () => {
+        console.log('hello ninja');
+    }
+
+    const handleclickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
+    const eventhandle = (name, e) => {
+        console.log('hello ' + name + 'e' + e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handleclick}>Click Me</button>
+            <button onClick={() => handleclickAgain('mithila')}>Click Me Again</button>
+            <button onClick={(e) => eventhandle('lishan', e)}>Click</button>
         </div>
     );
 }
